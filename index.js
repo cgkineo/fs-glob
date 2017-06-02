db1e9a3ae8f59c250a97519ba58b3a6d02ca8c83
Oliver Foster <oliver.foster@kineo.com>
2017-06-02 21:56:04 +0100

    Update

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-module.exports = require("./lib/api");
+"use strict";
+
+var util = require("./lib/util");
+var Globs = require("./lib/Globs");
+var Stat = require("./lib/Stat");
+var Stats = require("./lib/Stats");
+var Watch = require("./lib/Watch");
+var Watches = require("./lib/Watches");
+
+var api = function GlobsCreator(globs, location, pwd) { 
+    return new Globs(globs, location, pwd); 
+};
+
+util.extend(api, {
+
+    watches: util.watches,
+
+    posix: util.posix,
+    mkdir: util.mkdir,
+    abs: util.abs,
+    rel: util.rel,
+    home: util.home,
+    cwd: util.cwd,
+    norm: util.norm,
+    join: util.join,
+    stat: util.stat,
+
+    stats: function stats(options) {
+        return api(options).stats(options);
+    },
+
+    collate: function collate(options) {
+        return api(options).collate(options);
+    },
+
+    copy: function copy(options) {
+        return api(options).copy(options);
+    },
+
+    delete: function deleteFiles(options) {
+        return api(options).delete(options);
+    },
+
+    watch: function watch(options, callback) {
+        if (arguments.length < 2) {
+            return undefined;
+        }
+
+        var globs = api(options);
+        return globs.watch(callback, options);
+    },
+
+    play: function() {
+        api.watches.play();
+        return api;
+    },
+
+    pause: function pause() {
+        api.watches.pause();
+        return api;
+    },
+
+    stop: function stop() {
+        api.watches.unregister();  
+        return api;
+    }
+
+});
+
+module.exports = api;
